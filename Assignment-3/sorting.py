8a30f558a4dcc13908933825db2ba7683e58b543
Claire Gantan <clhg589@berkeley.edu>
2021-09-08 22:06:23 -0700

    finished sorting ex 5

--- Assignment-3/sorting.py
+++ Assignment-3/sorting.py
@@ -27,7 +27,7 @@ print(three_partition_sort([5, 10, 5, 20, 5, 5, 10], 1))
 #Sorting Exercise 3: Sorted Merge
 #You are given two sorted arrays, A and B, where A has a large enough buffer at the
 #end to hold B. Write a method to merge B into A in sorted order in one pass and using O(1) space.
-#start: 5:44
+#timing: 20 min
 def sorted_merge(a, b):
     #a[:] = a[0:len(a)-len(b)] + b[:]
     #initial approach was to merge both arrays and then sort, but that does not take advantage of the fact that both arrays are already sorted
@@ -50,6 +50,33 @@ def sorted_merge(a, b):
             ind_a += 1
             ind_b += 1
     return a
-
 print(sorted_merge([1,2,4,8,0,0,0],[1,3,5]))
-print(sorted_merge([1,2,8,0,0,0,0],[1,3,4, 5]))
+print(sorted_merge([1,2,8,0,0,0,0],[1,3,4,5]))
+
+
+#Sorting Exercise 4: Group Anagrams
+# Write a method to sort an array of strings so that all the anagrams are next to each other.
+# Assume the average length of the word as “k”, and “n” is the size of the array, where n >> k
+#  (i.e. “n” is very large in comparison to “k”). Do it in a time complexity better than O[n.log(n)]
+
+#Sorting Exercise 5: Peaks and Valleys
+# In an array of integers, a "peak" is an element that is greater than or equal to
+# the adjacent integers and a "valley" is an element that is less than or equal to
+# the adjacent integers. For example, in the array [5, 8, 6, 2, 3, 4, 6], [8, 6] are
+# peaks and [5, 2] are valleys. Given an array of integers, sort the array into an
+# alternating sequence of peaks and valleys.
+#timing: 13 min
+def peaks_and_valleys(arr):
+    #tried to use a window of 3 values to create an alternating peak and valley
+    i = 1
+    while i+1 < len(arr):
+        valley = min(arr[i-1], arr[i], arr[i+1])
+        if valley == arr[i+1]:
+             arr[i+1], arr[i] = arr[i], arr[i+1]
+        elif valley == arr[i-1]:
+            arr[i-1], arr[i] = arr[i], arr[i-1]
+        i += 2
+    return arr
+
+print(peaks_and_valleys([5,3,1,2,3]))
+print(peaks_and_valleys([6,6,7,2,3]))
